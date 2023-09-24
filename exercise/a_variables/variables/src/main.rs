9f70e55e9df5419a4ce99883d0007e5066073432
Andreas Sommer <andreas.sommer@student.tugraz.at>
2023-09-24 02:31:31 +0200

    a_variables

---
+++ exercise/a_variables/variables/src/main.rs
@@ -0,0 +1,13 @@
+const STARTIN_MISSILES:i32 = 8;
+const READY_AMOUNT:i32 = 2;
+
+fn main() {
+
+    let mut missiles = STARTIN_MISSILES;
+    let ready = READY_AMOUNT;
+
+    println!("Firing {} of my {} missiles...", ready, missiles);
+
+    missiles = missiles - ready;
+    println!("{} missiles left", missiles);
+}
